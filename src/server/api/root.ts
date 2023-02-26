b5ba03c0258f27f0674c8a3aba662968c8fb8216
zakiego <belummikir@gmail.com>
2023-02-27 00:26:55 +0800

    feat(trpc): add create update link

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,8 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
-
+import { prisma } from "~/src/server/db";
+import { linkRouter } from "~/src/server/api/routers/link";
+import { TEST_USER_ID } from "~/lib/constant";
 /**
  * This is the primary router for your server.
  *
@@ -8,7 +10,20 @@ import { exampleRouter } from "./routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  link: linkRouter,
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
+
+export const appTestCaller = appRouter.createCaller({
+  session: {
+    user: {
+      id: TEST_USER_ID,
+      email: "test@gmail.com",
+      name: "test",
+    },
+    expires: "9999-01-01T00:00:00.000Z",
+  },
+  prisma,
+});
