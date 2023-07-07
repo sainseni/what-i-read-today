5838bbaaabcca23070d3d5be22820c8c6d469785
Khoironi Kurnia Syah <khoironidev@gmail.com>
2023-07-07 08:46:44 +0800

    feat: custom error page

---
+++ src/components/ui/error-page.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import notFoundSvg from "~/public/svg/not-found.svg";
+
+interface ErrorPageProps {
+  svg?: typeof notFoundSvg;
+  message?: string;
+  children: React.ReactNode;
+}
+
+export const ErrorPage = ({
+  svg = notFoundSvg,
+  message = "Page Not Found",
+  children,
+}: ErrorPageProps) => {
+  return (
+    <>
+      <Head>
+        <title>{message}</title>
+      </Head>
+      <main className="flex items-center justify-center w-screen min-h-screen bg-white">
+        <div className="flex flex-col items-center max-w-lg gap-4 text-center">
+          <Image src={svg} alt="Not found" priority />
+          <h1 className="text-5xl font-bold">{message}</h1>
+          {children}
+        </div>
+      </main>
+    </>
+  );
+};
