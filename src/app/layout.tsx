83167b17803551fbd8459c359f90a7767e2052eb
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-11-20 13:10:25 +0700

    Refactor layout component to include Providers and Toaster components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import "~/src/styles/globals.css";
 import "@fontsource-variable/inter";
 
+import { Providers } from "~/src/app/providers";
+import { Toaster } from "~/src/components/ui/toaster";
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
