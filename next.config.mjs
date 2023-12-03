9cc9a75822d0dfa048036f3f4c988e91b186d119
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-12-03 19:37:08 +0800

    Remove TypeScript and ESLint configurations

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 export default config;
