25d7145a4e0d3dcf179153f5b24f1cc1330c66e5
M. Zakiyuddin Munziri <belummikir@gmail.com>
2023-12-03 21:01:48 +0800

    Add search functionality to user route

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,27 +1,80 @@
-import { and, desc, eq } from "drizzle-orm";
+import { and, desc, eq, like, or } from "drizzle-orm";
+import { count } from "drizzle-orm";
+import { type NextRequest } from "next/server";
+import { z } from "zod";
 
 import { dbPool } from "~/src/lib/db";
 import dbSchema from "~/src/lib/db/schema";
 
-export const dynamic = "force-dynamic"; // defaults to force-static
+export const dynamic = "force-dynamic";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } },
+  request: NextRequest,
+  context: { params: { id: string } },
 ) {
-  const id = params.id;
+  const { id } = context.params;
+  const searchParams = request.nextUrl.searchParams;
 
-  const data = await dbPool
-    .select({
-      title: dbSchema.link.title,
-      description: dbSchema.link.description,
-      url: dbSchema.link.url,
-      updatedAt: dbSchema.link.updatedAt,
-      createdAt: dbSchema.link.createdAt,
-    })
-    .from(dbSchema.link)
-    .where(and(eq(dbSchema.link.userId, id), eq(dbSchema.link.isPublic, true)))
-    .orderBy(desc(dbSchema.link.createdAt));
+  try {
+    const limit = z.coerce.number().parse(searchParams.get("limit") ?? 10);
+    const page = z.coerce.number().parse(searchParams.get("page") ?? 1);
+    const search = searchParams.get("search") ?? "";
 
-  return Response.json({ data });
+    const data = await dbPool
+      .select({
+        title: dbSchema.link.title,
+        description: dbSchema.link.description,
+        url: dbSchema.link.url,
+        updatedAt: dbSchema.link.updatedAt,
+        createdAt: dbSchema.link.createdAt,
+      })
+      .from(dbSchema.link)
+      .where(
+        and(
+          and(eq(dbSchema.link.userId, id), eq(dbSchema.link.isPublic, true)),
+          or(
+            like(dbSchema.link.title, `%${search}%`),
+            like(dbSchema.link.description, `%${search}%`),
+          ),
+        ),
+      )
+      .orderBy(desc(dbSchema.link.createdAt))
+      .limit(limit)
+      .offset((page - 1) * limit);
+
+    const [totalData] = await dbPool
+      .select({ value: count() })
+      .from(dbSchema.link)
+      .where(
+        and(
+          and(eq(dbSchema.link.userId, id), eq(dbSchema.link.isPublic, true)),
+          or(
+            like(dbSchema.link.title, `%${search}%`),
+            like(dbSchema.link.description, `%${search}%`),
+          ),
+        ),
+      );
+
+    if (!totalData) {
+      return Response.json({
+        error: "Not found",
+      });
+    }
+
+    const totalPage = Math.ceil(totalData.value / limit);
+
+    return Response.json({
+      data,
+      meta: {
+        limit,
+        page,
+        totalPage,
+      },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return Response.json({
+      error: error.message,
+    });
+  }
 }
